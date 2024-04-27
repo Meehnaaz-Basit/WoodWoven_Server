d53cf8b0ccf3003d5474add79a17ca3c85d7dd04
Meehnaaz Basit <mhnzbasit.0002@gmail.com>
2024-04-27 11:23:44 +0600

    added mondo db code and running the server successfully

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -9,6 +10,44 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Welcome to WoodWoven Server!");
+});
+
+// mongo
+// woodwoven
+// nMevX06I8m8NutyU
+
+const uri =
+  "mongodb+srv://woodwoven:nMevX06I8m8NutyU@cluster0.jzmmeq8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+// mongo ends
+
+app.get("/allCrafts", (req, res) => {
+  res.send("all craft here");
 });
 
 app.listen(port, () => {
